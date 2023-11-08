fbaa74295fd3a8aefa56fb3480db46078ecdfc0c
“SvnFrs” <thaidvqce170410@fpt.edu.vn>
2023-11-08 23:53:20 +0700

    Initial commit

---
+++ src/Body.js
@@ -0,0 +1,81 @@
+import React from "react";
+import "./Body.css";
+import Header from "./Header";
+import { useDataLayerValue } from "./DataLayer";
+import SongRow from "./SongRow";
+import { Favorite, MoreHoriz, PlayCircleFilled } from '@mui/icons-material';
+
+function Body({ spotify }) {
+  const [{ discover_weekly, device_id }, dispatch] = useDataLayerValue();
+
+  const playPlaylist = (id) => {
+    spotify
+      .play({
+        context_uri: `spotify:playlist:37i9dQZEVXcPwZFnVFdute`,
+      })
+      .then((res) => {
+        spotify.getMyCurrentPlayingTrack().then((r) => {
+          dispatch({
+            type: "SET_ITEM",
+            item: r.item,
+          });
+          dispatch({
+            type: "SET_PLAYING",
+            playing: true,
+          });
+        });
+      });
+  };
+
+  const playSong = (id) => {
+    spotify
+      .play({
+        device_id: "ea627bbace4e00bbf71804159361f638fe78aed0",
+        uris: [`spotify:track:${id}`],
+      })
+      .then((res) => {
+        spotify.getMyCurrentPlayingTrack().then((r) => {
+          dispatch({
+            type: "SET_ITEM",
+            item: r.item,
+          });
+          dispatch({
+            type: "SET_PLAYING",
+            playing: true,
+          });
+        });
+      });
+  };
+
+  return (
+    <div className="body">
+      <Header spotify={spotify} />
+
+      <div className="body__info">
+        <img src={discover_weekly?.images[0].url} alt="" />
+        <div className="body__infoText">
+          <strong>PLAYLIST</strong>
+          <h2>Discover Weekly</h2>
+          <p>{discover_weekly?.description}</p>
+        </div>
+      </div>
+
+      <div className="body__songs">
+        <div className="body__icons">
+          <PlayCircleFilled
+            className="body__shuffle"
+            onClick={playPlaylist}
+          />
+          <Favorite fontSize="large" />
+          <MoreHoriz />
+        </div>
+
+        {discover_weekly?.tracks.items.map((item) => (
+          <SongRow playSong={playSong} track={item.track} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Body;
