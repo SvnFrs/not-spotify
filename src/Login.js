2569318faea8c69b08a7a6e2c2d407df8ce1172f
“SvnFrs” <thaidvqce170410@fpt.edu.vn>
2023-11-09 03:07:07 +0700

    Improve login page

--- src/Login.js
+++ src/Login.js
@@ -5,11 +5,21 @@ import { loginUrl } from "./spotify";
 function Login() {
   return (
     <div className="login">
-      <img
-        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
-        alt=""
-      />
-      <a href={loginUrl}>LOGIN TO SPOTIFY</a>
+      <div className="navbar">
+          <img
+            src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_White.png"
+            alt="yolo"
+          />
+        </div>
+      <div className="container">
+        <div className="card">
+          <div className="card-header">
+            <h1>Log in to Spotify</h1>
+          </div>
+
+          <a href={loginUrl}>Log in</a>
+        </div>
+      </div>
     </div>
   );
 }
